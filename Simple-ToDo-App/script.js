6a5eeba98c1b09475022e69832ddc5983ecdffe3
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-06-27 17:41:39 +0300

    Update script.js

--- Simple-ToDo-App/script.js
+++ Simple-ToDo-App/script.js
@@ -5,3 +5,38 @@ const yyyy = date.getFullYear();
 const formattedDate = mm + '/' + dd + '/' + yyyy;
 const dateE = document.getElementById('date');
 dateE.innerHTML = formattedDate; 
+
+const input = document.getElementById('userInput');
+const addBtn = document.getElementById('addBtn');
+const ulE = document.getElementById('ulE');
+const listItemsC = document.getElementsByClassName('listItems');
+ 
+
+ function addTask(event) {
+            event.preventDefault();
+            let task = input.value;
+            listItemsC.style.display = "flex";
+            if (task !== "") {
+
+
+                const taskItem = document.createElement('li');
+                taskItem.innerHTML = `<span>${task}</span> <button class="deleteBtn">Delete</button>`;
+                ulE.appendChild(taskItem);
+
+                input.value = "";
+
+ 
+              const deleteBtn = document.getElementById('deleteBtn');
+                deleteBtn.addEventListener('click', deleteTask);
+            }
+            else {
+                alert("Error: must be text");
+            }
+        }
+
+          function deleteTask(event) {
+            const taskItemToDelete = event.target.parentElement;
+             ulE.removeChild(taskItemToDelete);
+        }
+
+        addBtn.addEventListener('click', addTask);
