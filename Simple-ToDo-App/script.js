8b1775c607c29325edd3c57dab45112bed061e11
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-06-29 20:02:30 +0300

    Update script.js

--- Simple-ToDo-App/script.js
+++ Simple-ToDo-App/script.js
@@ -10,20 +10,28 @@ const input = document.getElementById('userInput');
 const addBtn = document.getElementById('addBtn');
 const ulE = document.getElementById('ulE');
  
+let taskIndex = 0;
+let taskName = "task"
 
  function addTask() {
         
             let task = input.value;
              
             if (task !== "") {
+
+                localStorage.setItem(taskName + taskIndex.toString, task);
+                let taskKey = taskName + taskIndex.toString;
                 const listItemsC = document.getElementById('listItems');
                 const taskItem = document.createElement('li');
-                taskItem.innerHTML = `<span>${task}</span><button id="deleteBtn">Delete</button>`;
+                taskItem.innerHTML = `<span>${task}</span><button id="deleteBtn">Delete</button><span id="taskKey" style="display: none;">${taskKey}</span>`;
                 ulE.appendChild(taskItem);
 
                 input.value = "";
 
                 listItemsC.style.display = "flex";
+
+                 
+                taskIndex += 1; 
  
               const deleteBtn = document.getElementById('deleteBtn');
                 deleteBtn.addEventListener('click', deleteTask);
@@ -35,7 +43,11 @@ const ulE = document.getElementById('ulE');
 
           function deleteTask(event) {
             const taskItemToDelete = event.target.parentElement;
+            const taskKeyC = document.getElementById('taskKey');
+            const taskKey = taskKeyC.value;
              ulE.removeChild(taskItemToDelete);
+             localStorage.removeItem(taskKey.toString);
+             
         }
 
         addBtn.addEventListener('click', addTask);
